e782000e200fee6d22befff1a31c1fbf06d543f8
Daniel Strong <danielstrong@mac.com>
2025-02-03 15:15:29 -0900

    fix map rendering issue

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,21 +1,27 @@
-// src/components/Map.tsx
-import { MapContainer, Marker, Popup, TileLayer, Tooltip } from "react-leaflet"
-import "leaflet/dist/leaflet.css"
-import "leaflet-defaulticon-compatibility"
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+"use client";
 
-export default function MyMap(props: any) {
-  const { position, zoom } = props
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { LatLngExpression } from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-  return <MapContainer center={position} zoom={zoom} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
-}
+const position: LatLngExpression = [37.7749, -122.4194]; // Default location
+
+const Map = () => {
+  return (
+    <MapContainer
+      center={position}
+      zoom={10}
+      style={{ height: "500px", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+      <Marker position={position}>
+        <Popup>San Francisco</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
