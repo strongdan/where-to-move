2d1094827ac46530ae0abc25af5314d3fc125f03
Daniel Strong <danielstrong@mac.com>
2026-07-01 07:52:00 -0800

    Add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,29 @@ export const metadata: Metadata = {
   },
 };
 
+const googleTagManagerScript = `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M9NJ3S7C');
+`;
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <script>{googleTagManagerScript}</script>
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M9NJ3S7C"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <MicrosoftClarity />
         {children}
       </body>
